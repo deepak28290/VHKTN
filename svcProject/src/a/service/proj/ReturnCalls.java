0d69e2335d92bd49e1ddc21e3e82f11b93b3657d
Deepak Malhotra <deepak28290@gmail.com>
2014-03-30 02:46:50 +0530

    Update ReturnCalls.java

--- svcProject/src/a/service/proj/ReturnCalls.java
+++ svcProject/src/a/service/proj/ReturnCalls.java
@@ -1,9 +1,21 @@
 package a.service.proj;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.http.client.ClientProtocolException;
+import org.codehaus.jettison.json.JSONArray;
+
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 // Plain old Java Object it does not extend as class or implements 
 // an interface
@@ -15,13 +27,17 @@ import javax.ws.rs.core.MediaType;
 // The browser requests per default the HTML MIME type.
 
 //Sets the path to base URL + /hello
+@XmlRootElement
 @Path("/hello")
 public class ReturnCalls {
 
 	  @GET
-	  @Produces(MediaType.TEXT_PLAIN)
-	  public String getAllTweetsForUser() {
-	    return "Hello Jersey";
+	  @Produces(MediaType.APPLICATION_JSON)
+	  @Consumes(MediaType.APPLICATION_JSON)
+	  public HashMap<String, String> getAllTweetsForUser(String user) throws ClientProtocolException, IOException, TwitterException {
+		//  String json = new Gson().toJson(TwitterApiCalls.getTweetsFromUserName(user) );
+		 // JSONArray jsonA = JSONArray.fromObject(TwitterApiCalls.getTweetsFromUserName(user));
+		  return TwitterApiCalls.getTweetsFromUserName(user);
 	  }
 
 	  @GET
